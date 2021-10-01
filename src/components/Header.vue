becd71ed6afdaea3ab36d2c6cab38852e9c48613
Zarzuket <davide.bajo@gmail.com>
2021-10-01 14:56:53 +0200

    add style + components

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+      <header>
+          <div class="container_logo">
+              <img src="../assets/spotify-logo.png" alt="">
+          </div>
+      </header>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"Header"
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../style/variables";
+header{
+    background-color: $Header_Disk;
+    height: 3.125rem;
+    width: 100%;
+}
+
+.container_logo{
+    height: 100%;
+    
+    img{
+        height: 100%;
+        padding: .625rem;
+    }
+}
+
+</style>>
